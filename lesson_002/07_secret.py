03d655600ecd14627e421178737ff03e543a8eb6
VoKerrig <vokerrig@gmail.com>
2020-10-11 17:07:41 +0300

    Succesful

--- lesson_002/07_secret.py
+++ lesson_002/07_secret.py
@@ -20,5 +20,10 @@ secret_message = [
 #
 # Требуется задать конкретные индексы, например secret_message[3][12:23:4]
 # Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
-
-# TODO вывести расшифрованное сообщение
+first = secret_message[0][3]
+second = secret_message[1][9:13]
+third = secret_message[2][5:15:2]
+four = secret_message[3][12:6:-1]
+five = secret_message[4][20:15:-1]
+secret = first + second + third + four + five
+print(secret)
