60a66c1da1fe7dcd3574641be51170a53a4825ce
VoKerrig <vokerrig@gmail.com>
2020-10-11 14:09:49 +0300

    Succesful

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -20,7 +20,11 @@ violator_songs_list = [
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
-# TODO здесь ваш код
+track_4 = violator_songs_list[3][1]
+track_6 = violator_songs_list[5][1]
+track_9 = violator_songs_list[8][1]
+all = track_4 + track_6 + track_9
+print('Общее время звучания песен: Halo, Enjoy the Silence и Clean', all)
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -38,4 +42,8 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+track_2 = violator_songs_dict['World in My Eyes']
+track_7 = violator_songs_dict['Policy of Truth']
+track_8 = violator_songs_dict['Blue Dress']
+new_all = track_2 + track_7 + track_8
+print('Общее время звучания песен: Sweetest Perfection, Policy of Truth и Blue Dress', new_all)
