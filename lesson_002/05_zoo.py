6de131344468158157bc0b738c98fb4ea7014613
VoKerrig <vokerrig@gmail.com>
2020-10-11 12:48:58 +0300

    Succesful

--- lesson_002/05_zoo.py
+++ lesson_002/05_zoo.py
@@ -7,19 +7,25 @@ zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
 
 # посадите медведя (bear) между львом и кенгуру
 #  и выведите список на консоль
-# TODO здесь ваш код
+zoo.insert(1, 'bear')
+print(zoo)
 
 # добавьте птиц из списка birds в последние клетки зоопарка
 birds = ['rooster', 'ostrich', 'lark', ]
 #  и выведите список на консоль
-# TODO здесь ваш код
+new_zoo = zoo + birds
+print(new_zoo)
 
 # уберите слона
 #  и выведите список на консоль
-# TODO здесь ваш код
+new_zoo.remove('elephant')
+print(new_zoo)
 
 # выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть понятны простому человеку, не программисту.
-# TODO здесь ваш код
+jail_lion = new_zoo.index('lion') + 1
+jail_lark = new_zoo.index('lark') + 1
+print('Лев сидит в клетке под номером', jail_lion)
+print('Жаворонок сидит в клетке под номером', jail_lark)
 
 
