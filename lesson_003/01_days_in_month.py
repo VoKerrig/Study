056511f129baae7871b833599d3f7412d41398b8
VoKerrig <vokerrig@gmail.com>
2020-10-14 17:42:37 +0300

    Succesful

--- lesson_003/01_days_in_month.py
+++ lesson_003/01_days_in_month.py
@@ -9,6 +9,15 @@
 # Номер месяца получать от пользователя следующим образом
 user_input = input("Введите, пожалуйста, номер месяца: ")
 month = int(user_input)
-print('Вы ввели', month)
+# year = ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь']
+if month == 1 or month == 3 or month == 5 or month == 7 or month == 9 or month == 11:
+    print('31')
+elif month == 4 or month == 6 or month == 8 or month == 10 or month == 12:
+    print('30')
+elif month == 2:
+    print('28')
+else:
+    print('Номер месяца не распознан')
+# print('Вы ввели', month)
 
-# TODO здесь ваш код
+
