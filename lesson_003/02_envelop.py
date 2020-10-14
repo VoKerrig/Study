e4222988acb9784e52923330ed8d67919a3f0d97
VoKerrig <vokerrig@gmail.com>
2020-10-14 18:48:21 +0300

    problem solved

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -41,7 +41,7 @@ hole_x, hole_y = 8, 9
 # brick_x, brick_y, brick_z = 10, 2, 11
 # brick_x, brick_y, brick_z = 2, 10, 11
 # brick_x, brick_y, brick_z = 2, 11, 10
-brick_x, brick_y, brick_z = 3, 5, 6
+# brick_x, brick_y, brick_z = 3, 5, 6
 # brick_x, brick_y, brick_z = 3, 6, 5
 # brick_x, brick_y, brick_z = 6, 3, 5
 # brick_x, brick_y, brick_z = 6, 5, 3
@@ -49,8 +49,16 @@ brick_x, brick_y, brick_z = 3, 5, 6
 # brick_x, brick_y, brick_z = 5, 3, 6
 # brick_x, brick_y, brick_z = 11, 3, 6
 # brick_x, brick_y, brick_z = 11, 6, 3
-# brick_x, brick_y, brick_z = 6, 11, 3
+brick_x, brick_y, brick_z = 6, 11, 3
 # brick_x, brick_y, brick_z = 6, 3, 11
 # brick_x, brick_y, brick_z = 3, 6, 11
 # brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
+if hole_x >= brick_x and hole_y >= brick_y:
+    print('ДА')
+elif hole_x >= brick_z and hole_y >= brick_y:
+    print('ДА')
+elif hole_x >= brick_x and hole_y >= brick_z:
+    print('ДА')
+else:
+    print('НЕТ')
