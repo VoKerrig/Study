5e9f9bf1695ce9dbce03e7ea1ce315f3554d25f9
Vladimir <vokerrig@gmail.com>
2021-04-14 16:15:00 +0300

    Complete

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -19,8 +19,122 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+class Water:
 
+    def __init__(self):
+        self.name = "Вода"
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if type(other) == Air:
+            return Storm()
+
+
+class Air:
+
+    def __init__(self):
+        self.name = "Воздух"
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if type(other) == Fire:
+            return Lighting()
+        if type(other) == Earth:
+            return Dust()
+
+
+class Fire:
+
+    def __init__(self):
+        self.name = "Огонь"
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if type(other) == Water:
+            return Steam()
+        if type(other) == Earth:
+            return Lava()
+
+
+class Earth:
+
+    def __init__(self):
+        self.name = "Земля"
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if type(other) == Water:
+            return Dirt()
+
+
+class Storm:
+
+    def __init__(self):
+        self.name = "Шторм"
+
+    def __str__(self):
+        return self.name
+
+
+class Steam:
+
+    def __init__(self):
+        self.name = "Пар"
+
+    def __str__(self):
+        return self.name
+
+
+class Dirt:
+
+    def __init__(self):
+        self.name = "Грязь"
+
+    def __str__(self):
+        return self.name
+
+
+class Lighting:
+
+    def __init__(self):
+        self.name = "Молния"
+
+    def __str__(self):
+        return self.name
+
+
+class Dust:
+
+    def __init__(self):
+        self.name = "Пыль"
+
+    def __str__(self):
+        return self.name
+
+
+class Lava:
+
+    def __init__(self):
+        self.name = "Лава"
+
+    def __str__(self):
+        return self.name
+
+
+print(Water(), "+", Air(), "=", Water() + Air())
+print(Fire(), "+", Water(), "=", Fire() + Water())
+print(Earth(), "+", Water(), "=", Earth() + Water())
+print(Air(), "+", Fire(), "=", Air() + Fire())
+print(Air(), "+", Earth(), "=", Air() + Earth())
+print(Fire(), "+", Earth(), "=", Fire() + Earth())
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
 # Придумать что будет при сложении существующих элементов с новым.
